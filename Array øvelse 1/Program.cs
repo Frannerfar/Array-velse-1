5d4185bbba185b8c6398d1c5e5c940e2088cef0b
Franner22 <112959935+Franner22@users.noreply.github.com>
2025-09-15 10:38:53 +0200

    Add project files.

---
+++ Array øvelse 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays_øvelser_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mynumber = new int[] { 12, 33, 532, 145, 1, 4565, 1 };
+
+            mynumber[2] = 36534;
+            int temp = mynumber[0];
+
+            for (int i = 0; i < mynumber.Length; i++)
+                // ændring af 3 element 
+            {
+                Console.WriteLine(mynumber[i]);
+            }
+
+            string[] spillere = { "Anders", "Else", "hans", "kaj", "inge", "kurt", "tove" };
+
+        }
+    }
+}
